dde88c8cd5a515edae4f0cea83d84294e7a346c5
Abhay Singh <singhchirag188@gmail.com>
2024-02-07 16:20:08 +0530

    responsive nav bar

---
+++ Page 6 responsive navbar/script.js
@@ -0,0 +1,15 @@
+let openSideBar = document.querySelector('#openSideBar');
+let closeSideBar = document.querySelector('#closeSideBar');
+let sideBar = document.querySelector('#sidebar');
+
+openSideBar.addEventListener('click',()=>{
+    sideBar.style.transform = 'translateX(0px)';
+})
+
+document.body.addEventListener('click',()=>{
+    console.log('body clicked');
+})
+
+closeSideBar.addEventListener('click',()=>{
+    sideBar.style.transform = 'translateX(250px)';
+})
