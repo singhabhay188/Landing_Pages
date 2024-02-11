fb733399d7f38eb3e8a1d251d8ad059c85681e69
Abhay Singh <singhchirag188@gmail.com>
2024-02-11 17:37:52 +0530

    Pages 1 and 2 is ready

--- decodenew/script.js
+++ decodenew/script.js
@@ -1,16 +1,60 @@
+let smallCursor = document.querySelector('.smallcursor');
+
 let openSideBar = document.querySelector('#openSideBar');
 let closeSideBar = document.querySelector('#closeSideBar');
 let sideBar = document.querySelector('#sidebar');
 
-openSideBar.addEventListener('click',()=>{
-    sideBar.style.transform = 'translateX(0px)';
-})
+// openSideBar.addEventListener('click',()=>{
+//     sideBar.style.transform = 'translateX(0px)';
+// })
 
-document.body.addEventListener('click',()=>{
-    console.log('body clicked');
-})
+// document.body.addEventListener('click',()=>{
+//     console.log('body clicked');
+// })
 
-closeSideBar.addEventListener('click',()=>{
-    sideBar.style.transform = 'translateX(250px)';
-})
+// closeSideBar.addEventListener('click',()=>{
+//     sideBar.style.transform = 'translateX(250px)';
+// })
 
+
+
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.1,
+  });
+
+  Shery.makeMagnet("nav ul li", {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+  Shery.makeMagnet("#menu ul li", {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+  Shery.makeMagnet(".menu-icon", {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+  
+  Shery.makeMagnet("#logo" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+
+  function toggleMenu() {
+    var menu = document.getElementById('menu');
+    var menuIcon = document.querySelector('.menu-icon');
+    menu.classList.toggle('show');
+    if (menu.classList.contains('show')) {
+      menuIcon.textContent = '✖'; // Change icon to close
+    } else {
+      menuIcon.textContent = '☰'; // Change icon to menu
+    }
+  }
