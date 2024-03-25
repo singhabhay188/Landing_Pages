57e69ac70a5ba53efafab8e9702cc9c3f78afb85
Abhay Singh <singhchirag188@gmail.com>
2024-03-25 15:29:47 +0530

    Made some changes to first page.

--- Web d Course/webcourse3/script.js
+++ Web d Course/webcourse3/script.js
@@ -1,16 +1,6 @@
-const teams = document.querySelectorAll('.team');
-const movementScale = 0.01; // Adjust the movement scale as needed
-
-teams.forEach((team) => {
-    const imgContainer = team.querySelector('.img');
-    team.addEventListener('mousemove', (e) => {
-        const rect = team.getBoundingClientRect();
-        const centerX = rect.left + rect.width / 2;
-        const centerY = rect.top + rect.height / 2;
-        
-        let offsetX = centerX + (e.clientX - centerX) * movementScale;
-        let offsetY = centerY + (e.clientY - centerY) * movementScale;
-        
-        imgContainer.style.transform = `translate(-50%, -50%) translate(${offsetX}px, ${offsetY}px)`;
-    });
+gsap.to('.videodiv',{
+    scrollTrigger: {},
+    "--clip": "0%",
+    ease: "power2",
+    duration: 5,
 });
